1ba9d3795c38f235cc7edfa4f2dfb3215474d0e8
Kamil <kamilkolodziejczak95@gmail.com>
2023-01-12 20:22:56 +0100

    Completed challenge 15

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("home", {
+    homeStartingContent: homeStartingContent,
     homeStartingPosts: homeStartingPosts,
   });
 });
